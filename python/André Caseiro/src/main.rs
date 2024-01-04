8628061f8b23ba6a8710b30a9b3d596a5790538d
andrevcaseiro <andre.caseiro02@gmail.com>
2024-01-04 22:22:12 +0000

    Add command parser

--- python/André Caseiro/src/main.rs
+++ python/André Caseiro/src/main.rs
@@ -1,3 +1,52 @@
+use std::io::{self, Write};
+
+use clap::{command, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let mut cmd = cmd();
+
+    loop {
+        let input = prompt();
+
+        let matches = match cmd.try_get_matches_from_mut(input.split_whitespace()) {
+            Ok(matches) => matches,
+            Err(_) => {
+                println!("{}", cmd.render_help());
+                continue;
+            }
+        };
+
+        match matches.subcommand() {
+            Some(("exit", _)) => break,
+            Some(("help", _)) => println!("{}", cmd.render_help()),
+
+            _ => unreachable!(
+                "Exhausted list of subcommands and subcommand_required prevents `None`"
+            ),
+        }
+    }
+}
+
+/// Prompt the user for input and return the entered line.
+fn prompt() -> String {
+    print!("> ");
+    io::stdout().flush().expect("Failed to flush stdout");
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    input
+}
+
+// Build the CLI
+fn cmd() -> Command {
+    command!()
+        .no_binary_name(true)
+        .disable_help_flag(true)
+        .disable_version_flag(true)
+        .disable_help_subcommand(true)
+        .subcommand(Command::new("exit").about("Exit the app."))
+        .subcommand(Command::new("help").about("Exit the app."))
 }
